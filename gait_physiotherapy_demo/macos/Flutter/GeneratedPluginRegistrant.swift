9cadd87ae4bc45135057eda1a3ab34b9f5fd7070
maitry4 <maitryparikh23@gmail.com>
2026-07-13 19:03:15 +0530

    fixed

--- gait_physiotherapy_demo/macos/Flutter/GeneratedPluginRegistrant.swift
+++ gait_physiotherapy_demo/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -9,6 +9,7 @@ import battery_plus
 import device_info_plus
 import file_picker
 import flutter_blue_plus_darwin
+import flutter_llama
 import flutter_secure_storage_darwin
 import path_provider_foundation
 import printing
@@ -19,6 +20,7 @@ func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
+  FlutterLlamaPlugin.register(with: registry.registrar(forPlugin: "FlutterLlamaPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   PrintingPlugin.register(with: registry.registrar(forPlugin: "PrintingPlugin"))
